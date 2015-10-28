d70c0e6bffe4928e2f47ddf33e9e980d8d8a0f31
azeral <bondvsev@gmail.com>
2015-10-28 11:47:49 +0200

    Added LocalDate to Date and LocalDate to Timestamp converters

---
+++ core-api/src/main/java/net/bondar/webapp/api/converters/LocalDateTimeToTimestampConverter.java
@@ -0,0 +1,20 @@
+package net.bondar.webapp.api.converters;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+/**
+ * Created by oleksii on 10/10/15.
+ */
+@Converter(autoApply = true)
+public class LocalDateTimeToTimestampConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+    public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+        return localDateTime == null ? null : java.sql.Timestamp.valueOf(localDateTime);
+    }
+
+    public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
+        return timestamp == null ? null : timestamp.toLocalDateTime();
+    }
+}
